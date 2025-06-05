b353939170b24cdfd0a475d0be72dcd04584a95a
ashsandi <ashsandi@publicisgroupe.net>
2025-06-05 14:56:01 +0100

    updated vite.config.ts

--- apps/views/vite.config.ts
+++ apps/views/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,      // <- This is required so Render can reach the server
+    port: 10000      // <- Render scans this port by default
+  }
 })
