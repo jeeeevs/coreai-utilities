84f6b72c906efdb056c0f2af7ef379ff14b784cb
ashsandi <ashsandi@publicisgroupe.net>
2025-06-05 15:06:15 +0100

    updated vite.config.ts file

--- apps/views/vite.config.ts
+++ apps/views/vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    host: true,      // <- This is required so Render can reach the server
-    port: 10000      // <- Render scans this port by default
+    host: true,
+    port: 10000,
+    allowedHosts: ['haku-oo7-views.onrender.com'] // <- Hosted on Render
   }
 })
