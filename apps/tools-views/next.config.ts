5ec572779f56424a4c1f73e1a520343b3692f0ed
Ashmy <ashmy2000@hotmail.com>
2025-06-06 15:18:54 +0100

    updated folder structure

--- apps/tools-views/next.config.ts
+++ apps/tools-views/next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // Output build files to 'dist' instead of default '.next'
   distDir: 'dist',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.google.com',
+        pathname: '/s2/favicons/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
