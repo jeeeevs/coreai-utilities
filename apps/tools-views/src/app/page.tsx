d39dbc164092fe3109d492c618ebe8237ef9babb
Harshwardhan Goyal <harshwardhan.goyal@publicissapient.com>
2025-06-06 20:06:03 +0530

    update page.tsx

--- apps/tools-views/src/app/page.tsx
+++ apps/tools-views/src/app/page.tsx
@@ -2,19 +2,19 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100 p-4 flex flex-row gap-4">
       <iframe 
-        src="/agents/input-view"
+        src="/agents/research-agent/input-view"
         className="h-[600px] w-[600px] rounded-lg shadow-lg border-0"
         title="Research Input"
       />
 
       <iframe 
-        src="/agents/writting-agent"
+        src="/agents/writting-agent/output-view"
         className="h-[500px] w-[600px] rounded-lg shadow-lg border-0"
         title="Writing Agent"
       />
 
       <iframe 
-        src="/agents/image-generator"
+        src="/agents/image-generator/output-view"
         className="h-[500px] w-[600px] rounded-lg shadow-lg border-0"
         title="Image Generator"
       />
